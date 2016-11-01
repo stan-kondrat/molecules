ae58a1fac0349feb4956466b4bdf0a82ec55c18c
Stan Kondrat <stan.kondrat@gmail.com>
2016-11-01 03:49:17 +0300

    add molecule id,  improve collision,  crash method. Word process collisions

--- src/molecule.js
+++ src/molecule.js
@@ -2,9 +2,11 @@ import Point from './point';
 import Vector from './vector';
 
 const EPSILON = 1e-5;
+let _id = 0;
 
 class Molecule {
   constructor(point = new Point(), speed = new Vector(), radius = 0) {
+    this.id = _id++;
     this.position = point;
     this.speed = speed;
     this.radius = radius;
@@ -36,6 +38,62 @@ class Molecule {
    * @returns {Number|undefined} - distance
    */
   static collision(a, b, centersOnly){
+    // both not moving
+    if (a.speed.nullVector() && b.speed.nullVector()) {
+      return undefined;
+    }
+
+    if (Vector.collinear(a.speed, b.speed)) {
+      // set a position as origin
+      let Px = b.position.x - a.position.x;
+      let Py = b.position.y - a.position.y;
+      let P = new Vector(Px, Py);
+
+      let Vx;
+      let Vy;
+
+      if (!a.speed.nullVector()) {
+        Vx = a.speed.x;
+        Vy = a.speed.y;
+      } else if (!b.speed.nullVector()) {
+        Vx = b.speed.x;
+        Vy = b.speed.y;
+      }
+
+      let V = new Vector(Vx, Vy);
+      let angle = P.angleRadians() - V.angleRadians();
+
+      let distanCecenterLines = Math.abs(Math.sin(angle) * P.getLength()) ;
+
+      if ( distanCecenterLines > a.radius + b.radius) {
+        // will not collision, because speed vectors parallel, and spacing more than size of molecules
+        return undefined;
+      } else {
+        var time1 = (b.position.x - a.position.x)/(a.speed.x - b.speed.x);
+        var time2 = (b.position.y - a.position.y)/(a.speed.y - b.speed.y);
+        let centerTime;
+        if(isFinite(time1) && isFinite(time2)){
+          if (Math.abs(time1 - time2) < EPSILON) {
+            centerTime = time1;
+          }
+        } else if(isFinite(time1) && !isFinite(time2)) {
+          centerTime = time1;
+        } else if(!isFinite(time1) && isFinite(time2)) {
+          centerTime = time2;
+        }
+        if (centerTime > 0) {
+          let deltaTime = Math.cos(angle)*(a.radius + b.radius) / Vector.concat(a.speed, b.speed).getLength();
+          return centerTime - deltaTime;
+        } else {
+          return undefined;
+        }
+      }
+    }
+
+    if (!a.speed.x || !b.speed.x&& !a.speed.y && !b.speed.y) {
+      return undefined;
+    }
+
     // time when centerlines intersect
     var time1 = (b.position.x - a.position.x)/(a.speed.x - b.speed.x);
     var time2 = (b.position.y - a.position.y)/(a.speed.y - b.speed.y);
@@ -60,6 +118,26 @@ class Molecule {
     let timeAx = (Ax) / v;
     let timeCollision = time1 - timeAx;
     return timeCollision;
+  }
+
+  static crash(a, b) {
+    // U1 = (2 * m2 * V2 + (m1 - m2) * V1 ) / (m1 + m2);
+    // U2 = (2 * m1 * V1 + (m2 - m1) * V2 ) / (m1 + m2);
+
+    let m1 = a.radius;
+    let m2 = b.radius;
+    let V1x = a.speed.x;
+    let V2x = b.speed.x;
+    let V1y = a.speed.y;
+    let V2y = b.speed.y;
+    let U1x = (2 * m2 * V2x + (m1 - m2) * V1x ) / (m1 + m2);
+    let U1y = (2 * m2 * V2y + (m1 - m2) * V1y ) / (m1 + m2);
+    let U2x = (2 * m1 * V1x + (m2 - m1) * V2x ) / (m1 + m2);
+    let U2y = (2 * m1 * V1y + (m2 - m1) * V2y ) / (m1 + m2);
+    a.speed.x = U1x;
+    a.speed.y = U1y;
+    b.speed.x = U2x;
+    b.speed.y = U2y;
   }
 }
 
--- src/world.js
+++ src/world.js
@@ -4,24 +4,30 @@ class World {
   constructor() {
     this.molecules = [];
     this.time = 0;
+    this.nextCollisions = [];
+    this.nextCollisionsTime = null;
+    this.nextCollisionsExist = true;
   }
+
 
   /**
    * Detect time for next molecules collision
    * @returns {Number|undefined} - time
    */
-  nextCollisionTime() {
-    let times = [];
+  calcNextCollision() {
+    let minTime;
     for (let i = 0; i < this.molecules.length; i++) {
-      for (let j = 0; j < this.molecules.length; j++) {
-        if (i == j) continue;
-        let time = Molecule.collision(this.molecules[i], this.molecules[j], true);
-        if (time) {
-          times.push(time);
+      for (let j = i + 1; j < this.molecules.length; j++) {
+        let time = Molecule.collision(this.molecules[i], this.molecules[j]);
+        if (time > 0) {
+          if (!minTime || minTime > time){
+            minTime = time;
+            this.nextCollisions = [this.molecules[i], this.molecules[j]];
+          }
         }
       }
     }
-    return times.length ? Math.min.apply(null, times) : undefined;
+    return minTime + this.time;
   }
 
   moveMolecules(time) {
@@ -30,15 +36,31 @@ class World {
     }
   }
 
+  proccessCollision() {
+    Molecule.crash(this.nextCollisions[0], this.nextCollisions[1]);
+  }
+
   live(time) {
-    var collTime = this.nextCollisionTime();
-    if (collTime && collTime < time) {
-      this.time += collTime;
-      this.moveMolecules(collTime);
-      this.live(time - collTime);
-    } else {
+    if (this.nextCollisionsExist && !this.nextCollisionsTime) {
+      this.nextCollisionsTime = this.calcNextCollision();
+      this.nextCollisionsExist = !!this.nextCollisionsTime;
+    }
+
+    if (!this.nextCollisionsTime || this.nextCollisionsTime > this.time + time) {
       this.time += time;
       this.moveMolecules(time);
+    } else if(this.nextCollisionsTime == this.time + time) {
+      this.time += time;
+      this.moveMolecules(time);
+      this.proccessCollision();
+      this.nextCollisionsTime = this.calcNextCollision();
+    } else if(this.nextCollisionsTime < this.time + time) {
+      let timeToLive = this.nextCollisionsTime - this.time;
+      this.time += timeToLive;
+      this.moveMolecules(timeToLive);
+      this.proccessCollision();
+      this.nextCollisionsTime = this.calcNextCollision();
+      this.live(time - timeToLive);
     }
   }
 }

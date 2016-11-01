e194b5a76705fc3af93aac4aaacd7906971e46e2
Stan Kondrat <stan.kondrat@gmail.com>
2016-11-01 03:47:51 +0300

    add null vector

--- src/vector.js
+++ src/vector.js
@@ -14,11 +14,23 @@ class Vector {
     return Math.atan2(this.y, this.x) * 180 / Math.PI;
   }
 
+  nullVector() {
+    return this.x === 0 && this.y === 0;
+  }
+
+  getLength() {
+    return Math.hypot(this.x, this.y);
+  }
+
   static concat(a, b) {
     return new Vector(a.x + b.x, a.y + b.y);
   }
 
   static collinear(a, b) {
+    if (a.nullVector() || b.nullVector()) {
+      return true;
+    }
+
     let m = a.x / b.x;
     let n = a.y / b.y;
 
--- src/vector.spec.js
+++ src/vector.spec.js
@@ -23,16 +23,25 @@ describe('Vector class should', function() {
     assert.equal(vector3.y, 6);
   });
 
+  it('null vector', function() {
+    let vector1 = new Vector(0, 0);
+    let vector2 = new Vector(3, 4);
+    assert.equal(vector1.nullVector(), true);
+    assert.equal(vector2.nullVector(), false);
+  });
+
   it('check collinearity', function() {
     let vector1 = new Vector(0, 0);
     let vector2 = new Vector(0, 5);
     let vector3 = new Vector(3, 0);
     let vector4 = new Vector(3, 1);
     let vector5 = new Vector(6, 2.0000001);
+    let vector6 = new Vector(1, 0);
 
     assert.equal(Vector.collinear(vector1, vector4), true);
     assert.equal(Vector.collinear(vector2, vector3), false);
     assert.equal(Vector.collinear(vector3, vector4), false);
     assert.equal(Vector.collinear(vector4, vector5), true);
+    assert.equal(Vector.collinear(vector1, vector6), true);
   });
 })
